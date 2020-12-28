eb4b8b6c5da9b50b675004d95a104d5b1a74e53f
emeraldjava <paul.t.oconnell@gmail.com>
2020-12-28 14:06:06 +0000

    Add testing url details.

--- src/app/race/race.component.spec.ts
+++ src/app/race/race.component.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RaceComponent } from './race.component';
 
+// Links for testing components.
+// https://stackoverflow.com/questions/17370427/loading-a-mock-json-file-within-karmaangularjs-test
+// https://kaustubhtalathi.medium.com/mock-data-for-angular-5-applications-with-json-server-part-2-final-427bd68005bb
+// https://github.com/webslingur/angular-mock-data-part2
+// https://stackoverflow.com/questions/42129039/how-to-load-mock-data-from-json-file-in-angular-2-karma-jasmine-test?rq=1
 describe('RaceComponent', () => {
   let component: RaceComponent;
   let fixture: ComponentFixture<RaceComponent>;
